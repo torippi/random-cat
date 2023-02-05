9ca3de02505a8a8b200ab0688cb4d1f018873f8c
torippi <61878057+torippi@users.noreply.github.com>
2023-02-05 15:02:17 +0900

    test

---
+++ src/pages/index.tsx
@@ -0,0 +1,60 @@
+import { useEffect, useState } from "react";
+import type { NextPage, GetServerSideProps } from "next";
+
+interface CatCategory {
+    id: number;
+    name: string;
+}
+
+interface SearchCatImage {
+    breeds: string[];
+    categories: CatCategory[];
+    id: string;
+    url: string;
+    width: number;
+    height: number;
+}
+
+type SearchCatImageResponse = SearchCatImage[];
+
+const fetchCatImage = async (): Promise<SearchCatImage> => {
+    const res = await fetch("https://api.thecatapi.com/v1/images/search");
+    const result = (await res.json()) as SearchCatImageResponse;
+    return result[0];
+};
+
+interface IndexPageProps {
+    initialCatImageUrl: string;
+};
+
+const IndexPage: NextPage<IndexPageProps> = ({ initialCatImageUrl }) => {
+    const [catImageUrl, setCatImageUrl] = useState(initialCatImageUrl);
+
+    const handleClick = async () => {
+        const image = await fetchCatImage();
+        setCatImageUrl(image.url);
+    };
+
+    return (
+        <div>
+            <button onClick={handleClick}>きょうのにゃんこ🐱</button>
+            <div style={{ marginTop: 8 }}>
+                <img src={catImageUrl} />
+            </div>
+        </div>
+    );
+};
+
+export const getServerSideProps: GetServerSideProps<
+    IndexPageProps
+> = async () => {
+    const catImage = await fetchCatImage();
+    return {
+        props: {
+            initialCatImageUrl: catImage.url,
+        },
+    };
+};
+
+
+export default IndexPage;
